1cf9d2258dd00b33d31cc93017006a128ad54d65
jatinSinwaria8 <jatin.sinwaria@innoraft.com>
2025-03-28 16:23:48 +0530

    FRES-101: Task 2 completed.

--- index.php
+++ index.php
@@ -28,9 +28,20 @@
       include "./classes/" . $class . ".php";
     });
 
+    // upload dir path
+    const UploadDir = "./uploads";
+
+    // upload dir initilization
+    if (!is_dir(UploadDir)) {
+      mkdir(UploadDir, 0777, true);
+    }
+
     // creating firstname ,lastname 
     $firstname = new Name();
     $lastname = new Name();
+
+    // new picture object
+    $profile_picture = new Picture();
 
     // refining data comming from form input
     function datarefine($data)
@@ -46,11 +57,19 @@
 
       $firstname->name_validate($_POST["firstname"]);
       $lastname->name_validate($_POST["lastname"]);
+
+
+      // adding picture name, picture temp name
+      $profile_picture->picture_name = $_FILES['picture']['name'];
+      $profile_picture->picture_tempname = $_FILES['picture']['tmp_name'];
+      // adding picture path to picture object and moving uploaded picture to upload directory
+      $profile_picture->set_picture_path();
+      $profile_picture->move_picture();
     }
     ?>
 
     <!-- basic html form with input values of firstname, lastname, fullname and a submit button -->
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) ?>">
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) ?>" enctype="multipart/form-data">
 
       <!-- firstName input -->
       First Name : <input type="text" id="firstname" name="firstname">
@@ -65,6 +84,9 @@
       <!-- lastName input -->
       Full Name : <input type="text" id="fullname" name="fullname" disabled><br><br>
 
+      <!-- image input -->
+      Upload Image : <input type="file" id="picture" name="picture"><br><br>
+
       <!-- Submit button -->
       <input type="submit" value="Submit">
     </form>
@@ -72,13 +94,23 @@
     <!-- all fom outputs in this div -->
     <div class="formoutput">
 
+      <!-- form image output -->
+      <div class="formimage">
+        <?php
+
+        // showing uploaded image
+        if (!empty($profile_picture->picture_path)) {
+          echo "<img src=\"$profile_picture->picture_path\" alt=\"Uploaded Image\" title=\"Uploaded Image\" />";
+        }
+        ?>
+      </div>
+
       <!-- Full Name output -->
       <?php
       // showing hello [fullname]
       if (!empty($firstname->get_name_value()) && !empty($lastname->get_name_value())) {
         echo "<h1>Hello " . $firstname->get_name_value() . " " . $lastname->get_name_value() . "</h1>";
       }
-
       ?>
     </div>
   </div>
